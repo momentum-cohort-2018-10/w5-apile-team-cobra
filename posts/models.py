ce8e83401e97baa91f601a5f7d7d770af0463dda
xXDuckXx <dacs2010@gmail.com>
2018-11-26 17:34:25 -0500

    adds post model view url and template
    Co-authored-by iceviper1914 <mehiorob@gmail.com>

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    '''
+    post model
+    '''
+    title = models.CharField(max_length=255)
+    post_link = models.URLField(max_lenth=255)
+    
