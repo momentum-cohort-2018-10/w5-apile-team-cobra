df63e08d2ebb025c95d50896429d70ef15e22122
iceviper1914 <mehiorob@gmail.com>
2018-11-27 17:15:42 -0500

    Fixed Login

--- athletic_cave/urls.py
+++ athletic_cave/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
-    path('accounts/password/reset/done/', PasswordResetView.as_view(template_name='registration/password_reset_done.html'), name="password_reset_done"),
-	path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name="password_reset_confirm"),
-	path('accounts/password/done/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+    path('accounts/password/reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name="password_reset_done"),
+	path('accounts/password/reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name="password_reset_confirm"),
+	path('accounts/password/complete/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
     name="password_reset_complete"),
 ]
