96ff7d62e0b1bf9eb356dd9712f4dcfe81ecad39
xXDuckXx <dacs2010@gmail.com>
2018-11-26 19:28:04 -0500

    changes typo in models.URLField(maxlength), missing n

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
 class Post(models.Model):
     '''
     post model
     '''
     title = models.CharField(max_length=255)
-    post_link = models.URLField(max_lenth=255)
+    post_link = models.URLField(max_length=255)
     
