ffed2a703cfe2aa6bab0516d659c4152e62b040a
xXDuckXx <dacs2010@gmail.com>
2018-11-28 14:46:43 -0500

    posts current score displayed on home page

--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,7 @@ def index(request):
     '''
     render posts on home page
     '''
-    votes = Post.vote.all()
     posts = Post.objects.all()
     return render(request, 'index.html', {
         'posts': posts,
-        'votes': votes,
     })
