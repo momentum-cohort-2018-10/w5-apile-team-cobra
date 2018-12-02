4e477ed5cc9855ab8223f707ef412c25ff327e15
xXDuckXx <dacs2010@gmail.com>
2018-12-02 11:20:59 -0500

    eh

--- posts/management/commands/load-initial-data.py
+++ posts/management/commands/load-initial-data.py
@@ -1,11 +1,31 @@
 from django.core.management.base import BaseCommand
 
 
+intitial_posts = [
+    {
+        "title": "test",
+        "post_link": "www.google.com",
+        "user": "test",
+    }
+]
+
+
 class Command(BaseCommand):
-    help = "My shiny new management command."
+    help = "Imports the initial data and creates test users"
 
     def add_arguments(self, parser):
         pass
 
+    def create_posts(self):
+        print("deleted posts")
+        Post.objects.all().delete()
+
+        print("created posts")
+        posts = []
+
+        for post_data in intitial_posts:
+            
+
     def handle(self, *args, **options):
-        raise NotImplementedError()
+        
+                
