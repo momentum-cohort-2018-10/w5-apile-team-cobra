9eae2b955be439cd13c6b179d3cbe151d2095116
xXDuckXx <dacs2010@gmail.com>
2018-12-02 12:33:09 -0500

    create 10 users working

--- posts/management/commands/load-initial-data.py
+++ posts/management/commands/load-initial-data.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from posts.models import Post, Vote
 
 
 intitial_posts = [
@@ -16,16 +18,39 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
-    def create_posts(self):
-        print("deleted posts")
-        Post.objects.all().delete()
+    def create_users(self):
+        from mimesis import Person
+        print("deleted users")
+        # delete all the users except the admin
+        User.objects.filter(is_superuser=False).delete()
 
-        print("created posts")
-        posts = []
+        # create new users
+        print("created users")
+        users = []
+        person = Person()
+        
+        for _ in range(10):
+            # for instance of User model, create a person with...
+            user = User.objects.create_user(
+                person.username(), 
+                person.email(), 
+                "password"
+            )
+            # add user to users list    
+            users.append(user)
+        return users
 
-        for post_data in intitial_posts:
-            
+    # def create_posts(self):
+    #     print("deleted posts")
+    #     Post.objects.all().delete()
+
+    #     print("created posts")
+    #     posts = []
+
+    #     for post_data in intitial_posts:
+
 
     def handle(self, *args, **options):
+        users = self.create_users()
         
                 
